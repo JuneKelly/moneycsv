058907e147fdb93f50398ffc2819940b5f6d7664
June Kelly <june@cnjctr.com>
2023-04-11 18:27:04 +0100

    wip: more refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate yaml_rust;
 use std::{fs, path::PathBuf};
 
 use clap::Parser;
-use yaml_rust::{YamlLoader, Yaml};
+use yaml_rust::{Yaml, YamlLoader};
 
 const SEPARATOR: &str = ";";
 
@@ -24,24 +24,33 @@ fn print_row(row: Vec<&str>) {
     println!("{}", row.join(SEPARATOR));
 }
 
+fn flatten_rows(doc: &Vec<Yaml>) -> Vec<Vec<&str>> {
+    let rows: Vec<Vec<&str>> = doc
+        .iter()
+        .flat_map(|h| {
+            let hash = h.as_hash().unwrap();
+            hash.iter().flat_map(|(date, spends)| {
+                spends.as_vec().unwrap().iter().map(|spend| {
+                    let spend_str = spend.as_str().unwrap();
+                    let split: Vec<&str> = spend_str.splitn(2, ' ').collect();
+                    vec![date.as_str().unwrap(), split[0], split[1]]
+                })
+            })
+        })
+        .collect();
+    rows
+}
+
 fn main() {
     let args = Args::parse();
     let file_path = args.file;
 
     let docs = load_yaml_docs(file_path);
+
     let doc = &docs[0];
     let doc_vec = doc.as_vec().unwrap();
 
-    let rows = doc_vec.iter().flat_map(|h| {
-        let hash = h.as_hash().unwrap();
-        hash.iter().flat_map(|(date, spends)| {
-            spends.as_vec().unwrap().iter().map(|spend| {
-                let spend_str = spend.as_str().unwrap();
-                let split: Vec<&str> = spend_str.splitn(2, ' ').collect();
-                vec![date.as_str().unwrap(), split[0], split[1]]
-            })
-        })
-    });
+    let rows = flatten_rows(doc_vec);
 
     print_row(vec!["date", "cost", "description"]);
     for row in rows {
